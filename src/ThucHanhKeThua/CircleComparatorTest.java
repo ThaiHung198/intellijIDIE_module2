56e7e1d88043d615fcd9ac54cccbde41bf695b6a
thaihung198 <thaihung8503@gmail.com>
2025-04-15 11:01:27 +0700

    update  interface 'Comparator'

---
+++ src/ThucHanhKeThua/CircleComparatorTest.java
@@ -0,0 +1,23 @@
+package ThucHanhKeThua;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class CircleComparatorTest {
+    public static void main(String[] args) {
+        Circle[] circles = new Circle[3];
+        circles[0] = new ComparableCircle(3.6);
+        circles[1] = new ComparableCircle();
+        circles[2] = new ComparableCircle(3.5,"indigo",false);
+        System.out.println("Pre-sorted:");
+        for (Circle circle : circles) {
+            System.out.println(circle);
+        }
+        Comparator circleComparator = new CircleComparator();
+        Arrays.sort(circles, circleComparator);
+        System.out.println("After sort:");
+        for (Circle circle : circles) {
+            System.out.println(circle);
+        }
+    }
+}
